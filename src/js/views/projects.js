ddf7590e642d1f816125fa20aa9064cc59ac1f29
João Henrique Xavier <jhcxavier@gmail.com>
2019-05-30 14:12:00 +0000

    project style

--- src/js/views/projects.js
+++ src/js/views/projects.js
@@ -3,33 +3,33 @@ import React from "react";
 const projectDescription = [
 	{
 		name: "Hello World",
-		Tools: "React, CSS, HTML and JS",
+		Tools: "React · CSS · HTML · JS",
 		imgUrl: "https://via.placeholder.com/140x100"
 	},
 
 	{
 		name: "Hello World",
-		Tools: "React, CSS, HTML and JS",
+		Tools: "React · CSS · HTML · JS",
 		imgUrl: "https://via.placeholder.com/140x100"
 	},
 	{
 		name: "Hello World",
-		Tools: "React, CSS, HTML and JS",
+		Tools: "React · CSS · HTML · JS",
 		imgUrl: "https://via.placeholder.com/140x100"
 	},
 	{
 		name: "Hello World",
-		Tools: "React, CSS, HTML and JS",
+		Tools: "React · CSS · HTML · JS",
 		imgUrl: "https://via.placeholder.com/140x100"
 	},
 	{
 		name: "Hello World",
-		Tools: "React, CSS, HTML and JS",
+		Tools: "React · CSS · HTML · JS",
 		imgUrl: "https://via.placeholder.com/140x100"
 	},
 	{
 		name: "Hello World",
-		Tools: "React, CSS, HTML and JS",
+		Tools: "React · CSS · HTML · JS",
 		imgUrl: "https://via.placeholder.com/140x100"
 	}
 ];
@@ -38,12 +38,12 @@ class Projects extends React.Component {
 	render() {
 		return projectDescription.map((item, index) => {
 			return (
-				<div key={index} className="card projects col-3 md-4-mb-2 m-4">
+				<div key={index} className=" container-fluid card projects col-3 md-4-mb-2 m-4 p-0">
 					<img src={item.imgUrl} className="card-img-top" alt="..." />
 					<div className="card-body">
 						<h5 className="card-title">{item.name}</h5>
 						<p className="card-text">{item.Tools}</p>
-						<a href="#" className="btn btn-primary">
+						<a href="#" className="btn btn-primary float-left">
 							Demo
 						</a>
 						<a href="#" className="btn btn-primary float-right">
