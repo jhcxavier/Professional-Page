145008b465355dccf6cfc9aab52441f71335f784
VF4code <myemailaddress.bestcontact@gmail.com>
2019-05-31 02:56:48 +0000

    forms

--- src/js/views/forms.js
+++ src/js/views/forms.js
@@ -8,53 +8,45 @@ export default Projects;
 export class Home extends React.Component {
 	render() {
 		return (
-            import React from "react";
-export class Home extends React.Component {
-	render() {
-		return (
-<div>
-<div className="input-group mb-3">
-  <div className="input-group-prepend">
-    <span className="input-group-text" id="inputGroup-sizing-default">Full Name</span>
-  </div>
-  <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-</div>
 
-<div className="input-group mb-3">
-  <div className="input-group-prepend">
-    <span className="input-group-text" id="inputGroup-sizing-default">Job Title</span>
-  </div>
-  <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-</div>
-
-<div className="input-group mb-3">
-  <div className="input-group-prepend">
-    <span className="input-group-text" id="inputGroup-sizing-default">Skills</span>
-  </div>
-  <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-</div>
-
-<div className="input-group mb-3">
-  <div className="input-group-prepend">
-    <span className="input-group-text" id="inputGroup-sizing-default">Career Goal</span>
-  </div>
-  <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-</div>
-
-<div className="input-group mb-3">
-  <div className="input-group-prepend">
-    <span className="input-group-text" id="inputGroup-sizing-default">Mate All Day</span>
-  </div>
-  <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-</div>
-
-<div className="input-group mb-3">
-  <div className="input-group-prepend">
-    <span className="input-group-text" id="inputGroup-sizing-default">Fun In The Sun</span>
-  </div>
-  <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
-</div>
-</div>
-        )
+            <div>
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-default">Full Name</span>
+                    </div>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                </div>
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-default">Title</span>
+                    </div>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                </div>
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-default">Skills</span>
+                    </div>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                </div>
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-default">Goal</span>
+                    </div>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                </div>
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-default">Place Holder</span>
+                    </div>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                </div>
+                <div className="input-group mb-3">
+                    <div className="input-group-prepend">
+                        <span className="input-group-text" id="inputGroup-sizing-default">Place Holder</span>
+                    </div>
+                    <input type="text" className="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" />
+                </div>
+            </div>
+        )}
     }
-}
+
