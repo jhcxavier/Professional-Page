bcce1cd2fd2f073eafd495bfc11e172f718b72af
VF4code <myemailaddress.bestcontact@gmail.com>
2019-06-04 20:11:30 +0000

    login footer changes

--- src/js/component/loginFooter.js
+++ src/js/component/loginFooter.js
@@ -2,21 +2,6 @@ import React, { Component } from "react";
 
 export const loginFooter = () => (
 	<footer className="mt-auto py-3 pt-3 text-center">
-		<a className="li-ic mr-5" role="button">
-			<i className="fab fa-lg fa-linkedin-in" />
-		</a>
-		<a className="git-ic mr-5" role="button">
-			<i className="fab fa-lg fa-github" />
-		</a>
-		<a className="email-ic mr-5" role="button">
-			<i className="far fa-lg fa-envelope" />
-		</a>
-		<a className="tw-ic mr-5" role="button">
-			<i className="fab fa-twitter" />
-		</a>
-		<a className="ins-ic mr-5" role="button">
-			<i className="fab fa-instagram" />
-		</a>
 		<p>
 			<span>© 2019 **COMPANIES NAME** ALL RIGHTS RESERVED</span>
 		</p>
